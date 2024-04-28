c864691c514cd117068f08f5de4f90a382fb6b2d
Temurbek2211 <ftemur725@gmail.com>
2024-04-28 19:24:13 +0500

    My resume

---
+++ resume/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Salom</h1>
+
+      <button className='btn btn-danger mt-5'>Sign In</button>
+    </div>
+  );
+}
+
+export default App;
